044f23f53495cbddae1f1be0e190d483020ffe9e
Zadok Joshua <Ayegbazadok@outlook.com>
2022-10-29 05:55:11 +0100

    Updated

---
+++ BackendTaskStage1/DTO/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BackendTaskStage1.DTO
+{
+    public class UserDto
+    {
+        public string SlackUserName { get; set; } = string.Empty;
+        public bool Backend { get; set; }
+        public int Age { get; set; }
+        public string Bio { get; set; }
+    }
+}
